61110063d086aec0f15f4cbac35d296bc8bf3865
kopysovda <kopysovda@it2g.ru>
2023-02-20 23:05:30 +0400

    Добавьте файлы проекта.

---
+++ Market 1234/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Market_1234
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Market market = new Market();
+            market.Work();
+        }
+    }
+
+    class Market
+    {
+        private int _money = 0;
+        private static Random _random = new Random();
+        private Queue<Client> _clientsQueue = new Queue<Client>();
+
+        public Market()
+        {
+            CreateQueueClients();
+        }
+
+        public void CreateQueueClients()
+        {
+            int minValueClients = 5;
+            int maxValueClients = 10;
+
+            for (int i = 0; i < _random.Next(minValueClients, maxValueClients); i++)
+            {
+                _clientsQueue.Enqueue(new Client());
+            }
+        }
+
+        public void Work()
+        {
+            while (_clientsQueue.Count > 0)
+            {
+                Client client = _clientsQueue.Dequeue();
+                client.ShowInfo();
+                bool isActiv = true;
+
+                while (isActiv) 
+                {
+                    if(client.PaySuccess(client.GetBusket()))
+                    {
+                        _money += client.GetBusket().GetBusketSum();
+                        Console.WriteLine($"Баланс магазина {_money} рублей");
+                        isActiv = false;
+                    }
+                    else
+                    {
+                        Product product = client.GetBusket().GetProducts()[_random.Next(0, client.GetBusket().GetBusketCount())];
+
+                        Console.WriteLine($"У клиента {client.Money} денег. Не достаточно для совершения покупки, он убрал из корзины {product.Name} стоимостью {product.Cost}");
+
+                        client.GetBusket().RemoveProduct(product);
+
+                        if (client.PaySuccess(client.GetBusket()))
+                        {
+                            _money += client.GetBusket().GetBusketSum();
+                            Console.WriteLine($"Баланс магазина {_money} рублей");
+                            isActiv = false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    class Client
+    {
+        private int _money;
+        private Busket _basket;
+        private static Random _random = new Random();
+
+        public Client()
+        {
+            int minValueMoney = 100;
+            int maxValueMoney = 300;
+
+            _money = _random.Next(minValueMoney, maxValueMoney);
+            _basket = new Busket();
+        }
+
+        public int Money => _money;
+
+        public Busket GetBusket()
+        {
+            return _basket;
+        }
+
+        public bool PaySuccess(Busket busket)
+        {
+            if (busket.GetBusketCount() == 0)
+            {
+                Console.WriteLine("У улиента в корзине нет продуктов");
+                return false;
+            }
+
+            if (busket.GetBusketSum() <= _money)
+            {
+                Console.WriteLine($"Довольный клиент совершил покупку на {busket.GetBusketSum()}! Это успех");
+                _money -= busket.GetBusketSum();
+                return true;
+            }
+
+            return false;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"У клиента {_money} денег, стоимость корзины {_basket.GetBusketSum()}");
+
+            _basket.ShowBusket();
+        }
+    }
+
+    class Busket
+    {
+        private List<Product> _products = new List<Product>();
+        private static Random _random = new Random();
+
+        public Busket()
+        {
+            int maxValueProducts = 15;
+            int minValueProducts = 7;
+
+            for (int i = 0; i < _random.Next(minValueProducts, maxValueProducts); i++)
+            {
+                List<Product> products = new ProductCatalog().GetProducts();
+
+                _products.Add(products[_random.Next(products.Count)]);
+            }
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            if (_products.Count != 0)
+            {
+                _products.Remove(product);
+            }
+        }
+
+        public void ShowBusket()
+        {
+            foreach (var product in _products)
+            {
+                Console.WriteLine($"Продукт {product.Name} стоимость {product.Cost} рублей.");
+            }
+        }
+
+        public List<Product> GetProducts()
+        {
+            return new List<Product>(_products);
+        }
+
+        public int GetBusketSum()
+        {
+            int sumBusket = 0;
+
+            foreach (var item in _products)
+            {
+                sumBusket += item.Cost;
+            }
+
+            return sumBusket;
+        }
+
+        public int GetBusketCount()
+        {
+            return _products.Count;
+        }
+    }
+
+    class Product
+    {
+        public Product(string name, int cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+
+        public int Cost { get; }
+        public string Name { get; }
+    }
+
+    class ProductCatalog
+    {
+        private List<Product> _products = new List<Product>();
+
+        public ProductCatalog()
+        {
+            _products.Add(new Product("Презервативы", 50));
+            _products.Add(new Product("Молоко", 10));
+            _products.Add(new Product("Фанарик", 45));
+            _products.Add(new Product("Батончик", 12));
+            _products.Add(new Product("Хлеб", 5));
+            _products.Add(new Product("Сливки", 30));
+            _products.Add(new Product("Шоколад", 22));
+        }
+
+        public List<Product> GetProducts()
+        {
+            return new List<Product>(_products);
+        }
+    }
+}
---
+++ Market 1234/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Market 1234")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Market 1234")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c4c1187c-3cf5-45b7-a05a-5c53431803e4")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
